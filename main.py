e47bc1d1fbd89f17b592cbc931a27163b6bd4e84
makarov <makarov@tlpt.ru>
2024-03-14 15:46:51 +0400

    changes

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, executor, Dispatcher
+from aiogram import types
 
 bot = Bot(token="6959983203:AAHaLEB1U3YJkAXAxXFt2a9eTOmVt7YCRTU")
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
 admin_list = [641009347, 714611623]
 
 state = {}
 
 
-@dp.message(Command("start"))
+@dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     await message.answer(
         'Привет! Если ты ищешь поддержку, совет или просто кого-то, кто выслушает тебя анонимно, ты попал по адресу. Здесь ты можешь рассказать о своих проблемах, переживаниях или задать любой вопрос, не опасаясь осуждения. Так что не стесняйся делиться своими мыслями и чувствами - я здесь, чтобы помочь.')
 
 
-@dp.message(Command("anwser_to_message"))
+@dp.message_handler(commands="anwser_to_message")
 async def anwser_to_message(message: types.Message):
     state[message.chat.id] = {'state': 'anwser_to_message'}
     await message.answer("Введи циферки")
 
 
-@dp.message()
+@dp.message_handler()
 async def message_handler(message: types.Message):
     if message.chat.id in state.keys():
         if state[message.chat.id]['state'] == 'message_to_anwser':
@@ -42,9 +42,4 @@ async def message_handler(message: types.Message):
         await message.answer('Твое сообщение отправленно')
 
 
-async def main():
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+executor.start_polling(dp)
